da7a0717edb200748a58cf29404a047e32b6195e
shakil hossan <shakilhossan113@gmail.com>
2023-10-06 13:14:05 +0600

    second commit

---
+++ cardWar/cardWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardWarApp.swift
+//  cardWar
+//
+//  Created by MacBook Air on 23/9/23.
+//
+
+import SwiftUI
+
+@main
+struct cardWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
